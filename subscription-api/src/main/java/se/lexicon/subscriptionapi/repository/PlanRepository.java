754d5a928a54003337ad2f4ea1b6bb4296301595
Muthana Torsson <muthana.tursson@gmail.com>
2026-06-17 15:33:52 +0300

    Add plan and subscription repositories

---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/PlanRepository.java
@@ -0,0 +1,18 @@
+package se.lexicon.subscriptionapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+import se.lexicon.subscriptionapi.domain.entity.Plan;
+
+import java.util.List;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    List<Plan> findByActiveTrue();
+
+    List<Plan> findByActiveTrueAndServiceType(ServiceType serviceType);
+
+    List<Plan> findByOperatorIdAndActiveTrue(Long operatorId);
+}
---
+++ subscription-api/src/main/java/se/lexicon/subscriptionapi/repository/SubscriptionRepository.java
@@ -0,0 +1,24 @@
+package se.lexicon.subscriptionapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.lexicon.subscriptionapi.domain.constant.ServiceType;
+import se.lexicon.subscriptionapi.domain.constant.SubscriptionStatus;
+import se.lexicon.subscriptionapi.domain.entity.Subscription;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    List<Subscription> findByCustomerId(Long customerId);
+
+    Optional<Subscription> findByCustomerIdAndId(Long customerId, Long subscriptionId);
+
+    boolean existsByCustomerIdAndPlanServiceTypeAndStatus(
+            Long customerId,
+            ServiceType serviceType,
+            SubscriptionStatus status
+    );
+}
